79a90a5fffde9757e1c8a75e0b502f8c73779d6b
Dinesh Ganesan <dineshganesan@Dineshs-MBP-2.fios-router.home>
2019-12-09 21:29:45 -0500

    IAE-1 Updated Release Version

--- src/main/java/jptAutomation.java
+++ src/main/java/jptAutomation.java
@@ -2,9 +2,17 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.context.ApplicationContext;
+import com.launchdarkly.client.*;
+import java.io.IOException;
+import static java.util.Collections.singletonList;
+
+
 @RestController
 @EnableAutoConfiguration
 public class jptAutomation {
+
+	public static LDClient ldClient = null;
 
 	@RequestMapping("/")
 	String home() {
@@ -38,7 +46,24 @@ public class jptAutomation {
 	@RequestMapping(value = "/rel/version", method = RequestMethod.GET)
 	@ResponseBody
 	String getRelVersion() {
-		return "IAE New Version is : 25";
+
+			//LDUser user = new LDUser("dinesh@gsa.gov");
+   LDUser user = new LDUser.Builder("bob@example.com")
+                           .firstName("Bob")
+                           .lastName("Loblaw")
+                           .customString("groups", singletonList("beta_testers"))
+                           .build();
+
+			boolean showFeature = ldClient.boolVariation("immutable-demo", user, false);
+
+			if (showFeature) {
+			// application code to show the feature 
+				return "Rel. 24 (The feature flag is turned ON)";
+			}
+			else {
+			// the code to run if the feature is off
+				return "Rel. 24 (The feature flag is turned OFF)";
+			}
 	}
 
 	//IAE-2
@@ -80,8 +105,23 @@ public class jptAutomation {
 		return "Blue";
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(jptAutomation.class, args);
+	// public static void main(String[] args) {
+
+	// 	 SpringApplication.run(jptAutomation.class, args);
+	// }
+
+	public static void main(String[] args) throws IOException {
+
+			//ApplicationContext ctx = SpringApplication.run(jptAutomation.class, args);
+			LDClient ldClient = new LDClient("sdk-152bd4e4-5e04-439e-828b-1c8b4955d045");
+			//app.ldClient = ldClient;
+			jptAutomation.ldClient = ldClient;
+
+			SpringApplication.run(jptAutomation.class, args);
+
+			//ldClient.flush();
+			//ldClient.close();
+
 	}
 
 }
